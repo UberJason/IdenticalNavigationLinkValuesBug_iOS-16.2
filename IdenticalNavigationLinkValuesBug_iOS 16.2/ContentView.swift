badee2689e2108d1fd011e5ed4a7ad785334b1be
Jason Ji <uberjason@gmail.com>
2023-01-03 20:26:04 -0500

    Initial Commit

---
+++ IdenticalNavigationLinkValuesBug_iOS 16.2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  IdenticalNavigationLinkValuesBug_iOS 16.2
+//
+//  Created by Jason Ji on 1/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IdenticalNavigationLinkValuesBug_iOS 16.2/IdenticalNavigationLinkValuesBug_iOS_16_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IdenticalNavigationLinkValuesBug_iOS_16_2App.swift
+//  IdenticalNavigationLinkValuesBug_iOS 16.2
+//
+//  Created by Jason Ji on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct IdenticalNavigationLinkValuesBug_iOS_16_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
